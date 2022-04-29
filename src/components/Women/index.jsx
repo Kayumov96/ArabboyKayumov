50a06c107d6d84e0a5854911eff9460626798479
ArabboyKayumov <arabboykayumov@gmail.com>
2022-04-29 16:41:25 +0300

    card added to women products

--- src/components/Women/index.jsx
+++ src/components/Women/index.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
+import { women } from "../../utils/products";
+import { Card, CardImg, Container, Header } from "./style";
 
 class Women extends Component {
   render() {
     return (
-      <div>
-        <h1>Category name</h1>
-      </div>
+      <Container>
+        <Header>Category name</Header>
+        {/* <h1>Category name</h1> */}
+        {women.map((value) => (
+          <Card key={value.id}>
+            <CardImg src={value.src} />
+            <div>{value.title}</div>
+            <h4>{value.price}</h4>
+          </Card>
+        ))}
+      </Container>
     );
   }
 }
---
+++ src/utils/products.js
@@ -0,0 +1,13 @@
+import W1 from "../assets/img/W1.png";
+import W2 from "../assets/img/W2.png";
+import W3 from "../assets/img/W3.png";
+import W4 from "../assets/img/W4.png";
+
+export const women = [
+  { id: 1, title: "Apollo Running Short", src: W1, price: "50.00" },
+  { id: 2, title: "Apollo Running Short", src: W2, price: "50.00" },
+  { id: 3, title: "Apollo Running Short", src: W3, price: "50.00" },
+  { id: 4, title: "Apollo Running Short", src: W4, price: "50.00" },
+  { id: 5, title: "Apollo Running Short", src: W1, price: "50.00" },
+  { id: 6, title: "Apollo Running Short", src: W3, price: "50.00" },
+];
