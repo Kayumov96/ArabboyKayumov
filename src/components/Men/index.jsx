53cc7ba1eeb03ab230fc9971747b822d80de7c99
ArabboyKayumov <arabboykayumov@gmail.com>
2022-05-05 10:11:39 +0300

    men component issue fixed

--- src/components/Men/index.jsx
+++ src/components/Men/index.jsx
@@ -6,19 +6,34 @@ import { AddCard, Card, CardImg, Container, Header } from "./style";
 class Men extends Component {
   render() {
     return (
+      //   <Container>
+      //     <Header>Category name</Header>
+      //     {/* <h1>Category name</h1> */}
+      //     {data.map((value) => (
+      //       <Card key={value.id} className="card">
+      //         <CardImg src={value.src} />
+      //         <AddCard className="mini">
+      //           <CardImg cart src={Vectorcopy} alt="hey" />
+      //         </AddCard>
+      //         <Header mini>{value.title}</Header>
+      //         <h4>{value.price}</h4>
+      //       </Card>
+      //     ))}
+      //   </Container>
       <Container>
         <Header>Category name</Header>
-        {/* <h1>Category name</h1> */}
-        {data.map((value) => (
-          <Card key={value.id} className="card">
-            <CardImg src={value.src} />
-            <AddCard className="mini">
-              <CardImg cart src={Vectorcopy} alt="hey" />
-            </AddCard>
-            <Header mini>{value.title}</Header>
-            <h4>{value.price}</h4>
-          </Card>
-        ))}
+        {data.map((value) =>
+          value?.man?.map((val) => (
+            <Card key={val.id} className="card">
+              <CardImg src={val.src} alt="image" />
+              <AddCard className="mini">
+                <CardImg cart src={Vectorcopy} alt="hey" />
+              </AddCard>
+              <Header mini>{val.title}</Header>
+              <h4>{val.price}</h4>
+            </Card>
+          ))
+        )}
       </Container>
     );
   }
